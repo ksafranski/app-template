1b8446daa11ec2a8b17313f6edac3e04166ee380
fluidbyte <kent@fluidbyte.net>
2015-05-31 19:46:09 -0500

    Install lodash as dep

--- public/config.js
+++ public/config.js
@@ -18,6 +18,7 @@ System.config({
     "babel": "npm:babel-core@5.4.7",
     "babel-runtime": "npm:babel-runtime@5.4.7",
     "core-js": "npm:core-js@0.9.13",
+    "lodash": "npm:lodash@3.9.3",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
@@ -25,6 +26,9 @@ System.config({
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:lodash@3.9.3": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }
 });
